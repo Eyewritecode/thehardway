fc6a7714ad9c6ea3ad411cae17b07bb63f2adb57
EyeWriteCode <933kss@gmail.com>
2017-04-01 19:18:36 +0900

    printing

---
+++ ex6.rb
@@ -0,0 +1,17 @@
+x = "There are #{10} types if people"
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
