d61c1fe5129736f18c0b95beb7029d0f39c35718
EyeWriteCode <933kss@gmail.com>
2017-04-01 21:13:02 +0900

    unpacking

---
+++ ex13.rb
@@ -0,0 +1,7 @@
+first, second, third = ARGV
+
+puts "The script is called: #{$0}"
+puts "Your first variable is: #{first}"
+puts "Your secon variable is: #{second}"
+puts "Your third variable is: #{third}"
+
