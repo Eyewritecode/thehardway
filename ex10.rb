6fa577703b3f46dbb41919be7db9a6d95eeec8a5
EyeWriteCode <933kss@gmail.com>
2017-04-01 19:30:53 +0900

    printing

---
+++ ex10.rb
@@ -0,0 +1,16 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a like."
+backslash_cat = "I'm \\a \\ cat."
+
+fat_cat = <<MY_HEREDOC
+i'll o a list:
+\t .Cat Food
+\t .Fishies
+\t .Catnip\n\t .Grass
+MY_HEREDOC
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+
+puts fat_cat
