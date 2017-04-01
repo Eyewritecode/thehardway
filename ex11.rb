4b50c75c657606626b67634aa309309c7b095817
EyeWriteCode <933kss@gmail.com>
2017-04-01 19:43:41 +0900

    printing

---
+++ ex11.rb
@@ -0,0 +1,8 @@
+print "Nan sai desu ka "
+age = gets.chomp()
+print "How tall are you? "
+height = gets.chomp()
+print "How much do you weigh? "
+weight = gets.chomp()
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy"
